8cfa5c8f6c7b80f5f8bed42249188c0ec8310f2e
eric <yellowtry123@gmail.com>
2020-04-23 07:57:46 +0800

    add form candidates.new & token & AttributesERROR 50%

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -3,9 +3,21 @@ class CandidatesController < ActionController::Base
   end
   
   def new 
+  #  form表單的Candidate 帶到這邊 因為view屬於被動盡量讓controller來做 變數要到erb用需要讓他變成實體變數@
+   @candidate =  Candidate.new
   end
 
   def create
+    # 把candidate hash的資料整包丟進來(終端機看到的)
+    @candidate =  Candidate.new(params[:candidate])
+
+    if @candidate.save
+      # 成功
+      redirect_to '/candidates'
+    else
+
+    end
+
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
     #複數      符號    打開所有路徑七條路徑
   resources :candidates
+  
 end
 
 
