4d0e407e8d3fa3bf9c587a580da8a400f51e5600
eric <yellowtry123@gmail.com>
2020-05-05 23:53:50 +0800

    some pretty

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -12,9 +12,9 @@ class CandidatesController < ApplicationController
 
   def new 
   #  form表單的Candidate 帶到這邊 因為view屬於被動盡量讓controller來做 變數要到erb用需要讓他變成實體變數@
-   @candidate =  Candidate.new
+    @candidate =  Candidate.new
   end
-   
+
   def create
     # 2.把candidate hash的資料清洗過 帶進變數 clean_params 替換原本的params[:candidate]
     # clean_params = params.require(:candidate).permit(:name ,:party ,:age , :politics)
@@ -31,7 +31,7 @@ class CandidatesController < ApplicationController
     end
 
   end
-       
+      
   def  edit
     @candidate =  Candidate.find_by(id: params[:id])
   end
